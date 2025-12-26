f69cbdc710f9cb251d01b652958e178140a3bd0e
Fabimessi4 <139477474+fabimessi4v@users.noreply.github.com>
2025-12-26 18:33:30 -0300

    -archivos configuracion kubernetes

--- ms-prestamos/src/main/java/com/proyectogradle/msprestamos/Config/RestTemplateConfig.java
+++ ms-prestamos/src/main/java/com/proyectogradle/msprestamos/Config/RestTemplateConfig.java
@@ -3,11 +3,13 @@ package com.proyectogradle.msprestamos.Config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 
 @Configuration // Marks this class as a source of bean definitions
 public class RestTemplateConfig {
 
     @Bean // Provides a RestTemplate bean for injection
+    @LoadBalanced// Enables client-side load balancing
     public RestTemplate restTemplate() {
         return new RestTemplate();
     }
